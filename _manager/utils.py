6555f2e380131868293a11bad41abc6e122f0e1d
Gustavo Moreira <125270361+gusdev-r@users.noreply.github.com>
2024-11-30 20:27:25 -0300

    feat: utils to generate hateos links

--- _manager/utils.py
+++ _manager/utils.py
@@ -1,5 +1,6 @@
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.request import Request
 
 
 def response_app(data, status_code=200, exception=False, obj=None, links=None):
@@ -13,3 +14,28 @@ def response_app(data, status_code=200, exception=False, obj=None, links=None):
     if links is not None:
         response_data["links"] = links
     return Response(response_data, status_code)
+
+
+def generate_hateoas_links(
+    request: Request, resource_name: str, obj_id: int, request_type
+) -> dict:
+    base_url = request.build_absolute_uri(f"/{resource_name}/")
+    links = {}
+
+    if request_type == "get":
+        links["self"] = f"{base_url}{obj_id}/"
+        links["update"] = f"{base_url}{obj_id}/update/"
+        links["delete"] = f"{base_url}{obj_id}/delete/"
+
+    elif request_type == "post":
+        links["self"] = f"{base_url}{obj_id}/"
+        links["list"] = f"{base_url}"
+
+    elif request_type == "put":
+        links["self"] = f"{base_url}{obj_id}/"
+        links["delete"] = f"{base_url}{obj_id}/delete/"
+
+    elif request_type == "delete":
+        links["self"] = f"{base_url}{obj_id}/"
+
+    return links
