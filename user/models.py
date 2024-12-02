fe9563a2fd43307bbbc1c92c0609ed3e0145b10f
Gustavo Moreira <125270361+gusdev-r@users.noreply.github.com>
2024-12-01 23:18:03 -0300

    feat: model and migration

--- user/models.py
+++ user/models.py
@@ -1,12 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 import uuid
 
 
-class User(models.Model):
+class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     username = models.CharField(max_length=60)
     email = models.EmailField(unique=True)
-    password = models.CharField(max_length=128)
-    crated_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(default=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
