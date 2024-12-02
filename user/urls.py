569ea6dac14e7d19c03afb8cd7fbbab615777b2f
Gustavo Moreira <125270361+gusdev-r@users.noreply.github.com>
2024-12-01 23:20:07 -0300

    feat: user views and urls endpoints

---
+++ user/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("user/", views.UsersListView.as_view(), name="user-list"),
+    path("users/<str:user_id>/", views.UserDetailView.as_view(), name="user-detail"),
+    path("user/create/", views.UsersCreateView.as_view(), name="user-create"),
+    path(
+        "user/update/<str:user_id>/",
+        views.UsersUpdateView.as_view(),
+        name="user-update",
+    ),
+    path(
+        "user/delete/<str:user_id>/", views.UserDeleteView.as_view(), name="user-delete"
+    ),
+]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,155 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .serializers import UserSerializer
+from .models import User
+from _manager.utils import generate_hateoas_links, response_app
+from rest_framework import generics
+from rest_framework.views import status
+from rest_framework.response import Response
 
-# Create your views here.
+
+class UsersListView(APIView):
+    def get(self, request):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        links = generate_hateoas_links(
+            request=request, resource_name="user", obj_id=None, request_type="get"
+        )
+
+        return response_app(
+            data="List of users retrieved successfully",
+            obj=serializer.data,
+            links=links,
+        )
+
+
+class UsersListActiveView(APIView):
+    def get(self, request):
+        users = User.objects.filter(is_active=True)
+        serializer = UserSerializer(users, many=True)
+        links = generate_hateoas_links(
+            request=request, resource_name="user", request_type="get"
+        )
+
+        return response_app(
+            data="List of users retrieved successfully",
+            obj=serializer.data,
+            links=links,
+        )
+
+
+class UsersCreateView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+    def post(self, request):
+        try:
+            serializer = self.get_serializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            self.perform_create(serializer)
+            user_id = serializer.data.get("id")
+            links = generate_hateoas_links(
+                request=request,
+                obj_id=user_id,
+                resource_name="user",
+                request_type="post",
+            )
+
+            return response_app(
+                status_code=status.HTTP_201_CREATED,
+                data="User created successfully!",
+                obj=serializer.data,
+                links=links,
+            )
+
+        except Exception as exc:
+            return response_app(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                exception=True,
+                data=str(exc),
+            )
+
+    def options(self, request, *args, **kwargs):
+        allowed_methods = self.get_allowed_methods()
+        response = Response(
+            {
+                "message": "The allowed method for this route is POST.",
+                "allowed_methods": allowed_methods,
+            }
+        )
+        response["Allow"] = ", ".join(allowed_methods)
+        return response
+
+    def get_allowed_methods(self):
+        return ["POST"]
+
+
+class UserDetailView(APIView):
+    def get(self, request, user_id, *args, **kwargs):
+        print(user_id)
+        try:
+            user = User.objects.get(id=user_id)
+
+        except User.DoesNotExist:
+            return response_app(
+                status_code=status.HTTP_404_NOT_FOUND,
+                exception=True,
+                data="User not found",
+            )
+
+        serializer = UserSerializer(user)
+        links = generate_hateoas_links(
+            request=request, resource_name="user", obj_id=user_id, request_type="get"
+        )
+
+        return response_app(
+            data="User details retrieved successfully",
+            obj=serializer.data,
+            links=links,
+        )
+
+
+class UsersUpdateView(APIView):
+    def put(self, request, user_id, *args, **kwargs):
+        try:
+            user = User.objects.get(id=user_id)
+        except User.DoesNotExist:
+            return response_app(
+                "User not found", status_code=status.HTTP_404_NOT_FOUND, exception=True
+            )
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            links = generate_hateoas_links(
+                request=request,
+                resource_name="user",
+                obj_id=user_id,
+                request_type="put",
+            )
+
+            return response_app(
+                status_code=status.HTTP_201_CREATED,
+                data="User updated successfully",
+                obj=serializer.data,
+                links=links,
+            )
+        return response_app(
+            status_code=status.HTTP_400_BAD_REQUEST, exception=True, data="Invalid data"
+        )
+
+
+class UserDeleteView(APIView):
+    def delete(self, request, user_id, *args, **kwargs):
+        try:
+            user = User.objects.get(id=user_id)
+        except User.DoesNotExist:
+            return response_app(
+                "User not found", status_code=status.HTTP_404_NOT_FOUND, exception=True
+            )
+        user.delete()
+        links = generate_hateoas_links(request, "user", user_id, request_type="delete")
+
+        return response_app(
+            status_code=status.HTTP_204_NO_CONTENT,
+            data="User deleted successfully",
+            links=links,
+        )
